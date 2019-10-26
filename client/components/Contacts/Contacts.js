1416553dd2b34e9acddc67faefcef7fa9207a86b
kasperwhite <keriteabin@gmail.com>
2019-10-26 16:45:30 +0700

    FE: contacts modified

--- client/components/Contacts/Contacts.js
+++ client/components/Contacts/Contacts.js
@@ -1,24 +1,9 @@
 import React,{Component} from 'react';
-import { View, FlatList } from 'react-native';
-import { ListItem, Avatar, Icon, Button, SearchBar } from 'react-native-elements';
-import { ScrollView } from 'react-native-gesture-handler';
+import { View, FlatList, ScrollView, StyleSheet } from 'react-native';
+import { ListItem, Avatar, Icon, Button, SearchBar, Input } from 'react-native-elements';
 
 const ContactPanel = (props) => (
   <View style={{flexDirection: 'row'}}>
-    <Button
-      icon={
-        <Icon
-          name='comment'
-          type='font-awesome'
-          color='#193367'
-          size={17}
-        />
-      }
-      buttonStyle={{marginHorizontal: 3}}
-      containerStyle={{padding: 0}}
-      onPress={() => props.openDialog(props.userId)}
-      type='clear'
-    />
     <Button
       icon={
         <Icon
@@ -47,9 +32,17 @@ const AddContactButton = (props) => (
       />
     }
     buttonStyle={{marginHorizontal: 3}}
-    containerStyle={{padding: 0}}
-    onPress={() => props.addContact(props.userId)}
+    containerStyle={{
+      padding: 0,
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignContent: 'center',
+      width: '14%'
+    }}
+    onPress={() => props.addContact()}
     type='clear'
+    disabled={!props.search}
+    disabledStyle={{opacity: 0.6}}
   />
 )
 
@@ -60,27 +53,23 @@ class Contacts extends Component {
     this.state = {
       contacts: [
         {id: 0, username: 'Popovich', avatar: require('../../assets/cat.jpg')},
-        {id: 1, username: 'Popovich', avatar: require('../../assets/cat.jpg')},
-        {id: 2, username: 'Popovich', avatar: require('../../assets/cat.jpg')},
-        {id: 3, username: 'Popovich', avatar: require('../../assets/cat.jpg')},
-        {id: 4, username: 'Popovich', avatar: require('../../assets/cat.jpg')},
-        {id: 5, username: 'Popovich', avatar: require('../../assets/cat.jpg')},
+        {id: 1, username: 'Yaroslav', avatar: require('../../assets/cat.jpg')},
+        {id: 2, username: 'Vladimir', avatar: require('../../assets/cat.jpg')},
+        {id: 3, username: 'Amina', avatar: require('../../assets/cat.jpg')},
+        {id: 4, username: 'Ded', avatar: require('../../assets/cat.jpg')},
+        {id: 5, username: 'Denis', avatar: require('../../assets/cat.jpg')},
         {id: 6, username: 'Popovich', avatar: require('../../assets/cat.jpg')},
         {id: 7, username: 'Popovich', avatar: require('../../assets/cat.jpg')},
         {id: 8, username: 'Popovich', avatar: require('../../assets/cat.jpg')},
         {id: 9, username: 'Popovich', avatar: require('../../assets/cat.jpg')},
         {id: 10, username: 'Popovich', avatar: require('../../assets/cat.jpg')},
         {id: 11, username: 'Popovich', avatar: require('../../assets/cat.jpg')},
-        {id: 12, username: 'Popovich', avatar: require('../../assets/cat.jpg')},
-        
+        {id: 12, username: 'Popovich!!!!!!!!!', avatar: require('../../assets/cat.jpg')},
       ],
-      otherContacts: [
-        {id: 13, username: 'Popovich', avatar: require('../../assets/cat.jpg')},
-        {id: 14, username: 'Popovich', avatar: require('../../assets/cat.jpg')},
-        {id: 15, username: 'Popovich', avatar: require('../../assets/cat.jpg')}
-      ],
+      fContacts: [],
+      search: '',
       isLoading: false,
-      search: ''
+      isCancelVis: false
     }
   }
 
@@ -112,24 +101,40 @@ class Contacts extends Component {
     };
   }
 
-  updateSearch = search => {
-    this.setState({ search });
-  };
-
-  // OPEN DIALOG WITH USER
-  openDialog = (id) => {
-    console.log('Open Dialog', id);
+  componentDidMount(){
+    this.setState({fContacts: this.state.contacts});
   }
 
   // ADD CONTACT OPERATION
-  addContact = (id) => {
-    console.log('Add Contact', id);
+  addContact = () => {
+    console.log('Add Contact', this.state.search);
   }
 
   // DELETE CONTACT OPERATION
   deleteContact = (id) => {
     console.log('Delete', id);
   }
+
+  clearSearchField = () => {
+    this.setState({
+      search: '',
+      isCancelVis: false,
+      fContacts: this.state.contacts
+    })
+  }
+
+  updateSearch = search => {
+    if(!search){
+      this.setState({fContacts: this.state.contacts, isCancelVis: false});
+    } else {
+      const contacts = this.state.contacts;
+      this.setState({
+        fContacts: contacts.filter(el => el.username.includes(search)),
+        isCancelVis: true
+      })
+    }
+    this.setState({ search });
+  };
 
   renderContact = ({item, index}) => {
     return(
@@ -138,9 +143,10 @@ class Contacts extends Component {
         leftAvatar={{source: item.avatar}}
         bottomDivider
         rightElement={
-          this.state.contacts.find(el => el.id === item.id)
-          ? <ContactPanel openDialog={this.openDialog} deleteContact={this.deleteContact} userId={item.id}/>
-          : <AddContactButton addContact={this.addContact} userId={item.id}/>
+          <ContactPanel
+            deleteContact={this.deleteContact}
+            userId={item.id}
+          />
         }
       />
     )
@@ -148,31 +154,61 @@ class Contacts extends Component {
 
   render(){
     return(
+      <View style={{height: '100%', flexDirection: 'column'}}>
+        <View style={styles.searchCont}>
+          <Input
+            value={this.state.search}
+            placeholder='Find...'
+            /* rightIcon={
+              this.state.isCancelVis
+              ? <Button
+                icon={
+                  <Icon
+                    name='times'
+                    type='font-awesome'
+                    color='#193367'
+                    size={17}
+                  />
+                }
+                type='clear'
+                onPress={this.clearSearchField}
+              /> : null
+            } */
+            containerStyle={{width: '85%', borderWidth: 1, borderColor: 'red', borderRadius: 10}}
+            inputStyle={styles.searchInput}
+            inputContainerStyle={styles.searchInputCont}
+            onChangeText={(t) => this.updateSearch(t.replace(/\s/g,''))}
+            maxLength={20}
+          />
+          <AddContactButton addContact={this.addContact} search={this.state.search}/>
+        </View>
       <ScrollView>
-        <SearchBar
-          value={this.state.search}
-          placeholder='Search...'
-          onChangeText={this.updateSearch}
-          lightTheme={true}
-        />
         <FlatList
-          data={this.state.contacts}
+          data={this.state.fContacts}
           renderItem={this.renderContact}
           keyExtractor={i => i.id.toString()}
-        />
-        <FlatList
-          data={this.state.otherContacts}
-          renderItem={this.renderContact}
-          keyExtractor={i => i.id.toString()}
-          ListHeaderComponent={
-            <ListItem
-              title='Other'
-            />
-          }
+          contentContainerStyle={{paddingBottom: 20, marginVertical: 0}}
         />
       </ScrollView>
+      </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  searchCont: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignContent: 'center',
+    padding: 5,
+    backgroundColor: 'grey'
+  },
+  searchInput: {
+    padding: 5
+  },
+  searchInputCont: {
+    borderBottomWidth: 0
+  }
+})
 
 export default Contacts;
